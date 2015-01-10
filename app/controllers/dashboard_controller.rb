1c3a8de141132cddab57f3cde374139b762a31e0
stevegriffin <stephenmgriffin80@gmail.com>
2015-01-10 16:36:38 -0500

    basic dashboard functionality added

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,2 +1,20 @@
 class DashboardController < ApplicationController
+	before_action :get_projects, :set_user
+
+	#show.html.erb
+	def index	
+		binding.pry
+	end
+
+
+	private
+
+	def get_projects
+		@projects = Project.where(:user_id => params[:id])
+	end
+
+	def set_user
+		@user = User.find(params[:id])
+	end
+
 end
