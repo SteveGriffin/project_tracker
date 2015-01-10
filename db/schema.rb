b8a4710e5c2c4625126be320fdb2d1266ade9311
stevegriffin <stephenmgriffin80@gmail.com>
2015-01-10 15:50:52 -0500

    added dashboard controller

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,8 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150109205315) do
+ActiveRecord::Schema.define(version: 20150110204332) do
+
 
   create_table "projects", force: true do |t|
     t.string   "name"
