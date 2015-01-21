ac8fc4c856d200b3fb1183295c58eb276a7b59cc
stevegriffin <stephenmgriffin80@gmail.com>
2015-01-21 10:54:04 -0500

    Added total project time functionality

--- app/models/project.rb
+++ app/models/project.rb
@@ -45,4 +45,36 @@ class Project < ActiveRecord::Base
     @collaborations = Collaborator.find(user_id: user_id)
   end
 
+  #total time spent on the project, returns a hash of minutes and hours
+  def self.total_time(project_id)
+    @project = Project.find(project_id)
+    @sessions = @project.sessions
+    total = 0
+    @sessions.each do |session|
+      time = (session.end_time - session.start_time) / 60
+      total += time
+    end
+
+    #if less than an hours spent, pass back minutes and zero hours
+    if (total / 60) < 1
+      hours = 0
+      #round up to closest minute
+      minutes = total.ceil
+    else
+      #divmod returns the quotient and the remainder to get the hours and minutes
+      hours, minutes= total.divmod(60)
+      #round the minutes
+      minutes = minutes.ceil
+    end
+
+    #create and return hash of hours and minutes
+    result = Hash.new
+    result[:hours] = hours
+    result[:minutes] = minutes
+    #return result
+    result
+
+
+  end
+
 end
